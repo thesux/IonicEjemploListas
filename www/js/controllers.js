847978e2de817031b4c8d106e069b7187da14997
thesux <pop423@hotmail.com>
2016-05-19 15:56:00 -0500

    Lista estado y lista muncipio funcionando

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,32 @@ angular.module('starter.controllers', [])
 .controller('EjemploCtrl',function($scope, $rootScope,ws)
 {
 
+  $scope.onChange=function(id)
+  {
+    ws.getMunicipios(id).then(function(arrMunicipios)
+    {
+      $scope.municipios=arrMunicipios;
+      $scope.municipioSelect=$scope.municipios[0];
+    },function(error)
+    {
+      console.log(error);
+    });
+  }
 
    ws.getEstados().then(function(arrEstados)
    {
      $scope.estados=arrEstados;
-     $scope.estadoSelect=$scope.estados[0];
+     $scope.estadoSelect=$scope.estados[11];
+     ws.getMunicipios(11).then(function(arrMunicipios)
+     {
+       $scope.municipios=arrMunicipios;
+       $scope.municipioSelect=$scope.municipios[0];
+     },function(error)
+     {
+       console.log(error);
+     });
+
+
    },function(razon){
      console.error(razon);
    });
--- www/js/services.js
+++ www/js/services.js
@@ -15,6 +15,19 @@ angular.module('starter.services', [])
     return deferred.promise;
   }
 
+  function getMunicipios(id)
+  {
+    var deferred = $q.defer();
+    $http.get('http://eonproduccion.net/MobileApps/WS/03.ConfialeSecretario.php/estado/'+(id+1)+'/municipios')
+    .then(function(response){
+      deferred.resolve(response.data);
+    },function(razon){
+      deferred.reject(razon);
+    });
+    return deferred.promise;
+  }
 
-  return {getEstados: getEstados};
+
+  return {getEstados: getEstados,
+          getMunicipios: getMunicipios};
 })
